75fd432457614cd87f32288c5fce1e685929ece2
KJ <kjmiclea@gmail.com>
2026-02-24 15:48:00 -0800

    Added confirmation page with some incomplete JS

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// express import statement
 import express from 'express';
-
-// instance of app object
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 // server port
-const PORT = 3000;
+const PORT = 3003;
 
 // static file serving
 // tells express to use files in "public" directory
@@ -16,6 +15,14 @@ app.get('/', (req, res) => {
     // sends home.html file to client
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+app.post('/submit-order', (req, res) => {
+    const order = {
+
+    }
+
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+})
 
 // start server, listen on PORT
 app.listen(PORT, () => {
